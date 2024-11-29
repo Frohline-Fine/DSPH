3d9f944c80d9ddc13391b52c9786a5a0fce09d1a
Frohline-Fine <frohline.fine@gmail.com>
2024-11-29 22:30:18 +0100

    added QGridlayout and Button funcs

--- gui/test_gui.py
+++ gui/test_gui.py
@@ -7,10 +7,10 @@ from helper.get_exercise import get_exercise
 from helper.translator import translate
 
 import pandas as pd
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QSize
 from PyQt6.QtWidgets import (
     QDialog,
-    QApplication, QLabel, QVBoxLayout, QPushButton, )
+    QApplication, QLabel, QPushButton, QGridLayout, )
 
 path_to_csv = Path(__file__).parent.parent / 'data_extractor' / 'data' / 'test.csv'
 
@@ -23,34 +23,58 @@ class TrainDialog(QDialog):
         self.dataframe = pd.read_csv(path_to_csv)
         self.exercise = get_exercise(self.dataframe)
 
-        self.button = QPushButton("Zeige Antwort")
-        self.button.clicked.connect(self.on_click)
+        self.button_answer = QPushButton("Zeige Antwort")
+        self.button_answer.setIconSize(QSize(16, 16))
+        self.button_answer.clicked.connect(self.show_answer)
+
+        self.button_reset = QPushButton("Neue Frage")
+        self.button_reset.setIconSize(QSize(16, 16))
+        self.button_reset.clicked.connect(self.reset)
 
         self.label_question = QLabel(self.exercise[0])
         self.label_question.setFont(QFont('Arial', 12))
         self.label_question_e = QLabel(translate(self.label_question.text()))
         self.label_question_e.setFont(QFont('Arial', 12))
-        # label_question.setWordWrap(True)
+
         self.label_answer = QLabel(self.exercise[1])
         self.label_answer.setFont(QFont('Arial', 12))
+        self.label_answer.setHidden(True)
+
         self.label_explanation = QLabel(self.exercise[2])
         self.label_explanation.setFont(QFont('Arial', 12))
+        self.label_explanation.setHidden(True)
+
         self.label_explanation_e = QLabel(translate(self.exercise[2]))
         self.label_explanation_e.setFont(QFont('Arial', 12))
+        self.label_explanation_e.setHidden(True)
 
-        mein_layout = QVBoxLayout()
-        mein_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
-        mein_layout.setSpacing(20)
-        mein_layout.setContentsMargins(20, 20, 20, 20)
-        mein_layout.addWidget(self.label_question)
-        mein_layout.addWidget(self.label_question_e)
-        mein_layout.addWidget(self.label_answer)
-        mein_layout.addWidget(self.label_explanation)
-        mein_layout.addWidget(self.button)
+        main_layout = QGridLayout()
+        main_layout.setSpacing(20)
+        main_layout.setContentsMargins(20, 20, 20, 20)
 
-        self.setLayout(mein_layout)
+        main_layout.addWidget(self.label_question, 0, 0)
+        main_layout.addWidget(self.label_question_e, 0, 1)
 
-    def on_click(self):
+        main_layout.addWidget(self.label_answer, 1, 0)
+        main_layout.addWidget(self.label_explanation, 2, 0)
+        main_layout.addWidget(self.label_explanation_e, 2, 1)
+        main_layout.addWidget(self.button_answer, 3, 0)
+        main_layout.addWidget(self.button_reset, 3, 1)
+
+        self.setLayout(main_layout)
+
+    def show_answer(self):
+        self.label_answer.setHidden(False)
+        self.label_explanation.setHidden(False)
+        self.label_explanation_e.setHidden(False)
+        self.button_answer.setHidden(True)
+
+    def reset(self):
+        self.label_answer.setHidden(True)
+        self.label_explanation.setHidden(True)
+        self.label_explanation_e.setHidden(True)
+        self.button_answer.setHidden(False)
+
         self.exercise = get_exercise(self.dataframe)
         self.label_question.setText(self.exercise[0])
         self.label_question_e.setText(translate(self.exercise[0]))
