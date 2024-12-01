7525ae9103059dd36b17678ef560dd675fb0f773
Frohline-Fine <frohline.fine@gmail.com>
2024-12-01 21:44:35 +0100

    added waiting-function to bypass limitation. not tested because of it

--- helper/translator.py
+++ helper/translator.py
@@ -1,10 +1,16 @@
 """
 Translate text into english
 """
+import time
+import random
 from googletrans import Translator
+
+# generate a random delay between 1 and 5 seconds to bypass limitation
+delay = random.randint(1, 5)
 
 
 def translate(text):
     translator = Translator()
     translation = translator.translate(text, dest="en")
+    time.sleep(delay)
     return translation.text
