329ed1e9b06014bfba1511df6eb7bccd1847440e
Frohline-Fine <frohline.fine@gmail.com>
2024-12-02 10:53:32 +0100

    cleanup

--- helper/paths.py
+++ helper/paths.py
@@ -5,4 +5,3 @@ Path library
 from pathlib import Path
 
 src_file = Path(__file__).parent.parent / 'db' / 'source' / 'datei.pdf'
-# csv_file = Path(__file__).parent.parent / 'db' / 'data' / 'test.csv'
