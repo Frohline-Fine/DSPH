70007a841e6d981ebc7353a71946e7b213c95c4e
Frohline-Fine <frohline.fine@gmail.com>
2024-12-02 10:49:07 +0100

    changed waiting-function to bypass limitation from 1-5 to 1-3 and cleanup

--- db/init_funcs/clean_exercise.py
+++ db/init_funcs/clean_exercise.py
@@ -13,14 +13,8 @@ def cutter(string):
 
 # provide cleaned strings for QLabelWidgets
 def clean_exercise(problem):
-    # cleaned_problem = {'question': [], 'answer': [], 'explanation': []}
     question = cutter(str(problem[0]))
     answer = cutter(str(problem[1]))
     explanation = cutter(str(problem[2]))
 
-    # cleaned_problem['question'].append(cutter(str(problem[0])))
-    # cleaned_problem['answer'].append(cutter(str(problem[1])))
-    # cleaned_problem['explanation'].append(cutter(str(problem[2])))
-
     return question, answer, explanation
-    # return cleaned_problem
--- helper/translator.py
+++ helper/translator.py
@@ -6,7 +6,7 @@ import random
 from googletrans import Translator
 
 # generate a random delay between 1 and 5 seconds to bypass limitation
-delay = random.randint(1, 5)
+delay = random.randint(1, 3)
 
 
 def translate(text):
