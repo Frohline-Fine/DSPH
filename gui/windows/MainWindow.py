4f2edc2983fa1de2fe99466c2c0f53d77b5034ee
Frohline-Fine <frohline.fine@gmail.com>
2024-12-03 23:28:46 +0100

    added button widgets to MainWindow

--- gui/windows/MainWindow.py
+++ gui/windows/MainWindow.py
@@ -1,25 +1,68 @@
 import sys
 
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QApplication
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QApplication, QVBoxLayout, QWidget
 
+from gui.widgets.button import Button
 from gui.windows.TrainingWindow import TrainingWindow
-from helper.constants import MAIN
+from helper.constants import MAIN, BTN_TRAINING, BTN_EXAM, BTN_TRAINING_E, BTN_EXIT
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Hauptmenü")
-        self.setFixedSize(1440, 800)
+        self.setFixedSize(600, 600)
         self.setStyleSheet(f"background-color: rgb({MAIN});")
 
-        self.button = QPushButton("Öffne zweites Fenster")
-        self.button.clicked.connect(self.open_second_window)
+        self.button = QPushButton(BTN_TRAINING)
+        self.button.clicked.connect(self.open_training)
         self.setCentralWidget(self.button)
 
-    def open_second_window(self):
-        self.second_window = TrainingWindow()
-        self.second_window.show()
+        # Erstellen der Buttons
+        self.btn_training = Button(BTN_TRAINING)
+        self.btn_training.setFixedSize(QSize(400, 100))
+
+        self.btn_training_e = Button(BTN_TRAINING_E)
+        self.btn_training_e.setFixedSize(QSize(400, 100))
+
+        self.btn_exam = Button(BTN_EXAM)
+        self.btn_exam.setFixedSize(QSize(400, 100))
+
+        self.btn_exit = Button(BTN_EXIT)
+        self.btn_exit.setFixedSize(QSize(400, 100))
+
+        # Verbinden der Buttons mit Funktionen
+        self.btn_training.clicked.connect(self.open_training)
+        self.btn_training_e.clicked.connect(self.open_training_english)
+        self.btn_exam.clicked.connect(self.open_exam)
+        self.btn_exit.clicked.connect(self.exit_program)
+
+        # Layout erstellen und Buttons hinzufügen
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self.btn_training)
+        layout.addWidget(self.btn_training_e)
+        layout.addWidget(self.btn_exam)
+        layout.addWidget(self.btn_exit)
+
+        # Zentrales Widget erstellen und Layout zuweisen
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def open_training(self):
+        self.training_window = TrainingWindow()
+        self.training_window.show()
+
+    def open_training_english(self):
+        pass
+
+    def open_exam(self):
+        pass
+
+    def exit_program(self):
+        pass
 
 
 def main():
--- helper/constants.py
+++ helper/constants.py
@@ -1,19 +1,31 @@
 """
 Constants library
 """
-
-# RGB
-MAIN = '143,188,143'
-TRAINING = '143,188,143'
-LABEL = '245,255,250'
+""" DB """
 
 # DB
 DB_NAME = 'pruefungshelfer.sqlite'
 TABLE = 'exercises'
 
-# GUI
+""" GUI """
+
+# RGB
+MAIN = '143,188,143'
+TRAINING = '143,188,143'
+LABEL = '245,255,250'
+BUTTON_BG = '245,255,250'
+
+# MainWindow
+BTN_TRAINING = 'Übungsmodus'
+BTN_TRAINING_E = 'Trainingsmode'
+BTN_EXAM = 'Prüfungssimulation'
+BTN_EXIT = 'Exit'
+
+# TrainingWindow
 ANSWER = 'Lösung'
 NEXT = 'Weiter'
+
+""" DB Init """
 
 # divide_exercise.py
 CORRECT = 'Korrekte Antwort'
