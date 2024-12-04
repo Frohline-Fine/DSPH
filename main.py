cc84e87fbce4014b5bb58eabef071775d30b5f6f
Frohline-Fine <frohline.fine@gmail.com>
2024-12-04 16:38:09 +0100

    cleanup main.py

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
 # imports
-import sys
-
-from PyQt6.QtWidgets import QApplication
-
 from db.init import create_table
-from gui.test_gui import TrainDialog
+from gui.main_gui import main_gui
 
 if __name__ == '__main__':
     create_table()
-
-    app = QApplication(sys.argv)
-    main_dialog = TrainDialog()
-    main_dialog.show()
-    sys.exit(app.exec())
+    main_gui()
